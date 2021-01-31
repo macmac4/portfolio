d128d440cdb1f7a0b327d62104976d5dccdbcedd
Maciej B <maciej@galactica.pl>
2021-01-31 13:34:42 +0100

    add function manageRWD

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,18 +1,11 @@
 $(function() {
   $('.top-menu--button').click(function(){
     if ($(window).width() < 1145) {
-      if ($('.main-header').hasClass('active')) {
-        $('.main-header').removeClass('active');
-        $('.main-wrapper ').removeClass('active');
-      } else {
-        $('.main-header').addClass('active');
-        $('.main-wrapper ').addClass('active');
-      }
+      manageRWD();
     }
   });
 
   $('.pusher').click(function(){
-    console.log('clikc');
     if ($('.main-wrapper').hasClass('active')) {
       $('.main-header').removeClass('active');
       $('.main-wrapper').removeClass('active');
@@ -24,8 +17,22 @@ $(function() {
     $(this).addClass('active');
     let tag = $(this).data('link');
 
+    if ($(window).width() < 1145) {
+      manageRWD();
+    }
+
     $([document.documentElement, document.body]).animate({
         scrollTop: $(tag).offset().top
     }, 1000);
   });
 });
+
+function manageRWD() {
+  if ($('.main-header').hasClass('active')) {
+    $('.main-header').removeClass('active');
+    $('.main-wrapper ').removeClass('active');
+  } else {
+    $('.main-header').addClass('active');
+    $('.main-wrapper ').addClass('active');
+  }
+}
