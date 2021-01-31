6de11b5711510ce3b599eb4f6c9da847325693e9
Maciej B <maciej@galactica.pl>
2021-01-31 13:15:58 +0100

    create active link

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,6 +1,4 @@
 $(function() {
-  console.log( "ready!" );
-
   $('.top-menu--button').click(function(){
     if ($(window).width() < 1145) {
       if ($('.main-header').hasClass('active')) {
@@ -19,5 +17,15 @@ $(function() {
       $('.main-header').removeClass('active');
       $('.main-wrapper').removeClass('active');
     }
+  });
+
+  $(".menu--link .item").click(function() {
+    $(".menu--link .item").removeClass('active');
+    $(this).addClass('active');
+    let tag = $(this).data('link');
+
+    $([document.documentElement, document.body]).animate({
+        scrollTop: $(tag).offset().top
+    }, 1000);
   });
 });
